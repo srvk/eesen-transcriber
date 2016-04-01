363d58f753f69f5c5ddf3b6717be7d9d55955a24
riebling <er1k@cs.cmu.edu>
2016-04-01 14:54:25 -0400

    works independent of userid now
    Can't hard code -D
    Relies on vids2web.sh being on path (thanks to ~/bin pointing to ~/tools/eesen-offline-transcriber)

--- scripts/slurm.sh
+++ scripts/slurm.sh
@@ -3,7 +3,7 @@
 #SBATCH -s
 #SBATCH -n 1
 #SBATCH -o /vagrant/log/%j.log
-#SBATCH -D /home/vagrant/tools/eesen-offline-transcriber
+#SBATCH -D /tmp
 #SBATCH --get-user-env
 # runs vids2web.sh as a SLURM batch job
 
@@ -11,6 +11,6 @@ filename=$(basename "$1")
 basename="${filename%.*}"
 
 echo "Starting at `date`, in `pwd`"
-./vids2web.sh ${1}
+vids2web.sh ${1}
 
 echo "Done ($?) at `date`, ran on $SLURM_NODELIST ($SLURM_NNODES, $SLURM_NPROCS)"
